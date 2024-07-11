40b86f6701e91a59eaf5db13edaa60692200edf8
luis piñeiro <luis@localhost.localdomain>
2024-07-11 13:24:51 -0300

    uso de la misma variable de entorno en coin y derivates

--- src/app/coin/localComponent/functions/fechTable.ts
+++ src/app/coin/localComponent/functions/fechTable.ts
@@ -1,14 +1,14 @@
 "use server"
 import {jsonCoint, Coint} from "@/types/jsonCoin.types"
 
-const url = process.env.DOMAIN_URL;
+const url = process.env.NEXT_PUBLIC_URL_DERIBATE;
 const apicoint = "/api/coint";
 
 export const data = async (name: string | undefined): Promise<Coint[] | undefined> => {
   try {
   if (name === undefined) return undefined;
   const data = await fetch(`${url}${apicoint}?name=${name}`);
-    if(data.status === 500)  throw new Error()
+    if ( !data.ok || data.status === 500)  throw new Error()
   const json: jsonCoint = await data.json();
 
   return json.coints;
