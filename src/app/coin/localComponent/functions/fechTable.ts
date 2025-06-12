9fae910eca88986e44ae5f11dc757fa101c54c60
luis <luismateo3461@gmail.com>
2025-06-12 20:07:13 -0300

    arreglando page coin

--- src/app/coin/localComponent/functions/fechTable.ts
+++ src/app/coin/localComponent/functions/fechTable.ts
@@ -5,14 +5,14 @@ const url = process.env.NEXT_PUBLIC_URL_DERIBATE;
 const apicoint = "/api/coin";
 
 export const data = async (
-  name: string | undefined,
+  name: string | undefined
 ): Promise<Coint[] | undefined> => {
   if (name === undefined) {
     return undefined;
   }
   try {
     const data = await fetch(`${url}${apicoint}?name=${name}`);
-    if (data.ok === false || data.status === 500) {
+    if (!data.ok || data.status === 500) {
       throw new Error();
     }
     const json: jsonCoint = await data.json();
