98b363e1f081f056d4624d82282e5b28876c1867
Luis Mateo Piñeiro <luismateo3461@gmail.com>
2026-02-03 06:14:07 -0300

    vercion de nextjs cambiada, evita error en build

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,12 +1,12 @@
 import type { Meta, StoryObj } from "@storybook/nextjs";
 import { fn } from "storybook/test";
-//import { Button } from './Button';
-import { ButtonClient } from "@/app/derivatives/localcomponets/clientComponent/buttonClient";
+import { Button } from './Button';
+//import { ButtonClient } from "@/app/derivatives/localcomponets/clientComponent/buttonClient";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: "ButtonClient",
-  component: ButtonClient,
+  component: Button,
 
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
@@ -14,7 +14,7 @@ const meta = {
   },
   tags: ["autodocs"],
 
-} satisfies Meta<typeof ButtonClient>;
+} satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
@@ -24,6 +24,7 @@ export const Primary: Story = {
   args: {
     primary: true,
     label: "Button",
+
   },
 };
 
