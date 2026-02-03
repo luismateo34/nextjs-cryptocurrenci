82d3c0618a68964f3879144168a131738b05f251
Luis Mateo Piñeiro <luismateo3461@gmail.com>
2026-02-03 11:45:51 -0300

    table coint, cambio de estilos

--- src/app/coin/localComponent/cointList.tsx
+++ src/app/coin/localComponent/cointList.tsx
@@ -10,13 +10,16 @@ export const CointList = ({
     <>
       {Listcript.map((el) => {
         return (
-          <div key={el.id_coint} className="w-full h-7 grid grid-cols-3">
-            <div className="col-start-1 col-end-2 place-self-center overflow-hidden">
+          <div
+            key={el.id_coint}
+            className="w-full h-8 mr-2  grid grid-cols-3 hover:bg-white/30 rounded-md gap-3"
+          >
+            <div className="col-start-1 col-end-2 h-8 mr-2 flex justify-center place-self-center overflow-hidden w-full backdrop-blur-sm bg-black/30 rounded-md ">
               {el.name.length > 12
                 ? el.name.split(" ").slice(0, 2).join(" ")
                 : el.name}
             </div>
-            <div className="col-start-2 col-end-3 place-self-center overflow-hidden ">
+            <div className="col-start-2 col-end-3 h-8 mr-2 flex justify-center place-self-center overflow-hidden w-full backdrop-blur-sm bg-black/30 rounded-md">
               {el.symbol}
             </div>
             <div className="cols-start-3 col-end-4">
@@ -27,7 +30,7 @@ export const CointList = ({
                     ? el.name.split(" ").slice(0, 2).join(" ")
                     : el.name
                 }
-                className="w-full  h-8 mr-2 hover:bg-orange-600 active:bg-amber-500 "
+                className="w-full  h-8 mr-2 bg-indigo-800/90  hover:bg-orange-600 active:bg-amber-500  rounded-md"
               />
             </div>
           </div>
